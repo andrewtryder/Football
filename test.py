09a57527339f93e25eeb992871effcd058d5365a
spline <reticulatingspline@github.com>
2014-10-12 11:44:04 -0400

    Add in tests.

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 ###
-# Copyright (c) 2013, spline
+# Copyright (c) 2013-2014, spline
 # All rights reserved.
 #
 #
@@ -10,5 +10,8 @@ from supybot.test import *
 class FootballTestCase(PluginTestCase):
     plugins = ('Football',)
 
+    def testFootball(self):
+        self.assertError('footballchannel add #test') #, 'I have added SEC into #test')
+        self.assertError('footballchannel del #test') # , 'I have successfully removed SEC from #test')
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
