c0006fd81a2a23e8826c2c7b45e98d6b55cdfe2a
spline <reticulatingspline@github.com>
2014-10-12 11:47:47 -0400

    Update tests.

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ class FootballTestCase(PluginTestCase):
     plugins = ('Football',)
 
     def testFootball(self):
-        self.assertError('footballchannel add #test') #, 'I have added SEC into #test')
-        self.assertError('footballchannel del #test') # , 'I have successfully removed SEC from #test')
+        self.assertResponse('footballchannel add #test', "ERROR: '#test' is not a valid channel. You must add a channel that we are in.") #, 'I have added SEC into #test')
+        self.assertResponse('footballchannel del #test', "ERROR: '#test' is not a valid channel. You must add a channel that we are in.") #, 'I have added SEC into #test')
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
